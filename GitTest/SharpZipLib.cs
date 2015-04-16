b597fa60b9880bf402caacc325e9ddf78c8fb3dd
root <>
2015-04-16 09:28:11 +0200

    * Program.cs: * ZipUtils.cs: * SharpZipLib.cs: * GitTest.csproj:
    * asiis.cs: SharpZipLib Self-Extract

---
+++ GitTest/SharpZipLib.cs
@@ -0,0 +1,191 @@
+﻿
+using ZipOutputStream = ICSharpCode.SharpZipLib.Zip.ZipOutputStream;
+using ZipEntry = ICSharpCode.SharpZipLib.Zip.ZipEntry;
+using ZipExeBytes = ICSharpCode.SharpZipLib.Zip.ZipExeBytes;
+
+
+// Create a Zip as a browser download attachment in IIS
+namespace ZipUtils
+{
+
+
+    class SharpZipLib
+    {
+
+
+		public static void xxx()
+		{
+			ICSharpCode.SharpZipLib.Zip.FastZip fastZip = new ICSharpCode.SharpZipLib.Zip.FastZip();
+
+			fastZip.CreateEmptyDirectories = true;
+			// fastZip.Password = "foobar";
+
+
+			// Include all files by recursing through the directory structure
+			bool recurse = true; 
+
+			// Dont filter any files at all 
+			string filter = null;
+
+			// System.IO.Stream strm = System.Web.HttpContext.Current.Response.OutputStream;
+			// fastZip.CreateZip (strm, "sourced", true, null);
+
+			fastZip.CreateZip("fileName.zip", @"C:\SourceDirectory", recurse, filter);
+		}
+
+
+
+		/// <summary>
+		/// Function to create a selfextracting archive file
+		/// </summary>
+		/// <param name="zipFilename">the zip that u want to create an exe from</param>
+		/// <param name="outputName">the name of the exe to create</param>
+		public static void CreateExe(string zipFilename, string outputName)
+		{
+			//check for valid 
+			if (outputName == null)
+			{
+				outputName = System.IO.Path.ChangeExtension(zipFilename, ".exe");
+			}
+			else if (outputName.Length == 0)
+			{
+				outputName = System.IO.Path.ChangeExtension(zipFilename, ".exe");
+			}
+
+
+			//make sure it ends with the exe extention
+			if (!outputName.EndsWith(".exe"))
+			{
+				outputName = outputName + ".exe";
+			}
+
+			//make the two filestreams needed for reading zip and writing exe
+			System.IO.FileStream Zip = new System.IO.FileStream(
+				zipFilename
+				, System.IO.FileMode.Open
+				, System.IO.FileAccess.Read
+				, System.IO.FileShare.Read
+			);
+
+			System.IO.FileStream exe = new System.IO.FileStream(
+				outputName
+				, System.IO.FileMode.Create
+				, System.IO.FileAccess.Write
+			);
+
+			int ReadIn = 0, chunksize = 2048;
+			byte[] buffer = new byte[chunksize];
+			//write the startercode for the exe
+			exe.Write(ZipExeBytes.exe, 0, ZipExeBytes.exe.Length);
+			//start reading the zipfile
+			ReadIn = Zip.Read(buffer, 0, chunksize);
+			//add the zipp in the extention of the starter
+			while (ReadIn > 0)
+			{
+				exe.Write(buffer, 0, ReadIn);
+				ReadIn = Zip.Read(buffer, 0, chunksize);
+			}
+
+			//close the zip
+			Zip.Close();
+
+			//close the exe
+			exe.Close();
+
+			//null it out
+			buffer = null;
+		}
+
+
+
+
+
+		// http://blogs.msdn.com/b/dotnetinterop/archive/2008/06/04/dotnetzip-now-can-save-directly-to-asp-net-response-outputstream.aspx
+
+        // This will accumulate each of the files named in the fileList into a zip file,
+        // and stream it to the browser.
+        // This approach writes directly to the Response OutputStream.
+        // The browser starts to receive data immediately which should avoid timeout problems.
+        // This also avoids an intermediate memorystream, saving memory on large files.
+        //
+        public static void DownloadZipToBrowser(System.Collections.Generic.List<string> zipFileList)
+        {
+			System.Web.HttpResponse Response = System.Web.HttpContext.Current.Response;
+
+			Response.ClearContent();
+			Response.ClearHeaders();
+			Response.Clear();
+
+			Response.Buffer = false;
+
+            Response.ContentType = "application/zip";
+            // If the browser is receiving a mangled zipfile, IIS Compression may cause this problem. Some members have found that
+            //    Response.ContentType = "application/octet-stream"     has solved this. May be specific to Internet Explorer.
+
+            Response.AppendHeader("content-disposition", "attachment; filename=\"Download.zip\"");
+            // Response.CacheControl = "Private";
+            // Response.Cache.SetExpires(System.DateTime.Now.AddMinutes(3)); // or put a timestamp in the filename in the content-disposition
+
+			// http://stackoverflow.com/questions/9303919/pack-empty-directory-with-sharpziplib
+
+
+            byte[] buffer = new byte[4096];
+
+			using (ZipOutputStream zipOutputStream = new ZipOutputStream (Response.OutputStream))
+			{
+				zipOutputStream.SetLevel (3); //0-9, 9 being the highest level of compression
+
+				// zipOutputStream.Dispose
+
+				foreach (string directoryName in zipFileList)
+				{
+					string dname = "myfolder/";
+
+					// ZipEntry entry = new ZipEntry (ZipEntry.CleanName (fileName));
+					// entry.Size = fs.Length;
+				}
+
+
+				foreach (string fileName in zipFileList)
+				{
+					// or any suitable inputstream
+					using (System.IO.Stream fs = System.IO.File.OpenRead (fileName))
+					{
+
+						ZipEntry entry = new ZipEntry (ZipEntry.CleanName (fileName));
+						entry.Size = fs.Length;
+
+                
+						// Setting the Size provides WinXP built-in extractor compatibility,
+						// but if not available, you can set zipOutputStream.UseZip64 = UseZip64.Off instead.
+						zipOutputStream.PutNextEntry (entry);
+
+						int count = fs.Read (buffer, 0, buffer.Length);
+						while (count > 0)
+						{
+							zipOutputStream.Write (buffer, 0, count);
+							count = fs.Read (buffer, 0, buffer.Length);
+
+							if (!Response.IsClientConnected)
+								break;
+
+							Response.Flush ();
+						} // Whend 
+
+						fs.Close ();
+					} // End Using fs 
+
+				} // Next fileName
+
+				zipOutputStream.Close ();
+			} // End Using zipOutputStream 
+
+            Response.Flush();
+            Response.End();
+		} // End Function DownloadZipToBrowser
+
+
+	} 
+
+
+}
---
+++ GitTest/ZipUtils.cs
@@ -0,0 +1,158 @@
+﻿
+namespace ZipUtils
+{
+
+
+	public class DotNetZip
+	{
+
+		// zzzzzzz
+		// http://blogs.msdn.com/b/dotnetinterop/archive/2008/06/04/dotnetzip-now-can-save-directly-to-asp-net-response-outputstream.aspx
+		public void ZipToHttpResponse(string DirectoryToZip)
+		{
+			string ReadmeText= "This is a zip file dynamically generated at " + System.DateTime.Now.ToString("G");
+
+			System.Web.HttpResponse Response = System.Web.HttpContext.Current.Response;
+			Response.Clear();
+
+			string fileName = @"Довнлоад.zip"; // @"Довнлоад.зип"
+			fileName = @"Download.zip";
+
+			Response.ContentType = "application/zip";
+			Response.AddHeader("content-disposition", "filename=" + fileName);
+
+			//using (Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile(Response.OutputStream)) 
+			using (Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile(System.Text.Encoding.UTF8))
+			{
+				// Add to the zip archive, the file selected in the dropdownlist.
+				// zip.AddFile(ListOfFiles.SelectedItem.Text, "files");
+				// Add a boilerplate copyright file into the zip.
+				// zip.AddFile(@"\static\Copyright.txt", "");
+				// The string ReadmeText becomes the content for an entry in the zip archive, with the filename "Readme.txt".
+				// zip.AddStringAsFile(ReadmeText, "Readme.txt", "");
+
+				zip.Comment = "This will be embedded into a self-extracting exe";
+				zip.AddEntry("ReadMeZB.txt", ReadmeText);
+
+				// http://dotnetslackers.com/articles/aspnet/Use-ASP-NET-and-DotNetZip-to-Create-and-Extract-ZIP-Files.aspx
+				// zip.Password = "Rumpelstilzchen";
+				// zip.Encryption = Ionic.Zip.EncryptionAlgorithm.PkzipWeak;
+
+				//zip.StatusMessageTextWriter = System.Console.Out;
+				zip.AddDirectory(DirectoryToZip); // recurses subdirectories
+
+				// zip.Save();
+				zip.Save(Response.OutputStream);
+			} // End Using zip
+
+			Response.End();
+		} // End Sub ZipToHttpResponse 
+
+
+		public static void ZipWithDotNetZip()
+		{
+
+			using (Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile())
+			{
+				// add this map file into the "images" directory in the zip archive
+				zip.AddFile("c:\\images\\personal\\7440-N49th.png", "images");
+				// add the report into a different directory in the archive
+				zip.AddFile("c:\\Reports\\2008-Regional-Sales-Report.pdf", "files");
+				zip.AddFile("ReadMe.txt");
+				zip.Save("MyZipFile.zip");
+			} // End Using zip
+
+		} // End Sub ZipWithDotNetZip 
+
+
+		public static void ZipAutoExtract()
+		{
+
+			using (Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile())
+			{
+				// zip up a directory
+				zip.AddDirectory("C:\\project1\\datafiles", "data");
+
+				zip.Comment = "This will be embedded into a self-extracting exe";
+				zip.AddEntry("Readme.txt", "This is content for a 'Readme' file that will appear in the zip.");
+				zip.SaveSelfExtractor("archive.exe", Ionic.Zip.SelfExtractorFlavor.ConsoleApplication);
+			} // End Using zip 
+
+		} // End Using ZipAutoExtract 
+
+
+		public static void MyExtractProgress(object sender, Ionic.Zip.ExtractProgressEventArgs e)
+		{
+
+		}
+
+
+		// public delegate void ExtractProgress_t(object sender, Ionic.Zip.ExtractProgressEventArgs e);
+
+
+		public static void Unzip(string zipToUnpack, string targetDir)
+		{
+			// Unzip (zipToUnpack, targetDir, MyExtractProgress);
+			Unzip (zipToUnpack, targetDir, null);
+		}
+
+
+		// Unzip using DotNetZip
+		// Unzip("abc.zip", "/foo/bar");
+		public static void Unzip(string zipToUnpack, string targetDir, System.EventHandler<Ionic.Zip.ExtractProgressEventArgs> extractProgress)
+		{
+			System.Console.WriteLine("Extracting file {0} to {1}", zipToUnpack, targetDir);
+			using (Ionic.Zip.ZipFile zip = Ionic.Zip.ZipFile.Read(zipToUnpack))
+			{
+				if(extractProgress != null)
+					zip.ExtractProgress += extractProgress;
+				// here, we extract every entry, but we could extract    
+				// based on entry name, size, date, etc.   
+				foreach (Ionic.Zip.ZipEntry e in zip)
+				{
+					e.Extract(targetDir, Ionic.Zip.ExtractExistingFileAction.OverwriteSilently);
+				}
+			}
+
+		}
+
+
+
+		// http://stackoverflow.com/questions/1578823/creating-a-zip-extractor
+		public static void CreateSelfExtra()
+		{
+			string DirectoryToZip = "";
+			string ZipFileToCreate = "";
+
+			using (Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile())
+			{
+				zip.Comment = "This will be embedded into a self-extracting console-based exe";
+				//zip.StatusMessageTextWriter = System.Console.Out;
+				zip.AddDirectory(DirectoryToZip); // recurses subdirectories
+				//zip.Password = "foobar";
+				//zip.Encryption = Ionic.Zip.EncryptionAlgorithm.WinZipAes256;
+				//zip.Save(ZipFileToCreate);
+
+
+				Ionic.Zip.SelfExtractorSaveOptions options = new Ionic.Zip.SelfExtractorSaveOptions();
+				options.Flavor = Ionic.Zip.SelfExtractorFlavor.ConsoleApplication;
+				//options.DefaultExtractDirectory = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), System.IO.Directory.GetParent(ZipFileToCreate).Name);
+				options.DefaultExtractDirectory = "%TEMP%";
+				options.ExtractExistingFile = Ionic.Zip.ExtractExistingFileAction.OverwriteSilently;
+
+				// http://dotnetzip.codeplex.com/workitem/10682
+				//options.IconFile = System.IO.Path.Combine(Application.StartupPath, "box_software.ico");
+				//options.PostExtractCommandLine = "putty.exe";
+				//options.Quiet = true;
+				//options.RemoveUnpackedFilesAfterExecute = true;
+
+				zip.SaveSelfExtractor(System.IO.Path.ChangeExtension(ZipFileToCreate, ".exe"), options);
+			} // End Using zip
+
+		} // End Sub CreateSelfExtra
+
+
+	} // End Class 
+
+
+} // End Namespace 
