e42e0a47e1f198be50db67939cf7ed05f68c3ab7
ststeiger <quandary82@hailmail.net>
2015-04-20 12:05:23 +0200

    Removed files

--- WebTest/Properties/AssemblyInfo.cs
+++ WebTest/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Revisions- und Buildnummern 
 // übernehmen, indem Sie "*" wie folgt verwenden:
-[assembly: AssemblyVersion("2015.4.16.1037")]
-[assembly: AssemblyFileVersion("2015.4.16.1037")]
+[assembly: AssemblyVersion("2015.4.20.1204")]
+[assembly: AssemblyFileVersion("2015.4.20.1204")]
